07e08c6c676dfc5e64eea60fe2caa2fedd0f946f
ima_wizzard <aitor.vannevel@gmail.com>
2023-03-05 16:37:45 +0100

    Fixed Python API

--- PythonAPI/flaskapi.py
+++ PythonAPI/flaskapi.py
@@ -10,15 +10,12 @@ def main():
 
 @api.route('/color', methods=['POST', 'GET'])
 def test():
-    print("receive", datetime.datetime.today())
     json = request.get_json()
     red = "&R=" + json["red"]
     green = "&G=" + json["green"]
     blue = "&B=" + json["blue"]
     url = 'http://auroraWLED.local/win' + red + green + blue
-    print("receive", datetime.datetime.today())
     requests.post(url)
-    print("send", datetime.datetime.today())
     return render_template('index.html')
 
 if __name__ == '__main__':
