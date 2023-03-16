f6b1c6f882012d84d2dde9c6ca4028e2b78cea30
ima_wizzard <aitor.vannevel@gmail.com>
2023-03-16 11:17:12 +0100

    update website

--- website/javascript/script.js
+++ website/javascript/script.js
@@ -12,30 +12,15 @@ function Connect(){
         api: document.getElementById("api").value,
         wled: document.getElementById("wled").value
     }
-    console.log("connect");
     Post("connect", data);
 }
 
 function Post(page, data){
-    console.log("http://" + document.getElementById("api").value + "/" + page);
     fetch("http://" + document.getElementById("api").value + "/" + page, {
         method: 'post',
         body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json'
         }
-    })  .then(response => {
-        //handle response
-        info = response.headers.get("info")
-        console.log(response);
-        state = document.getElementById("stateWled");
-        if(info != null && info == "wrong_url"){
-            state.innerText = "Unable to connect\nWrong url";
-            state.style.color = "red";
-        }
-        if(info != null && info == "connected"){
-            state.innerText = "Connect to " + data["wled"];
-            state.style.color = "green";
-        }
-      })      
+    })    
 }
