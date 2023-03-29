186c23e443d5aab8e910853a4efd5979ccc1958d
ima_wizzard <aitor.vannevel@gmail.com>
2023-03-29 18:17:39 +0200

    added mosquitto

--- PythonAPI/API/aurora_sender.py
+++ PythonAPI/API/aurora_sender.py
@@ -5,7 +5,7 @@ import time
 import json
 from threading import Thread, Timer
 
-broker = 'Mosquitto'
+broker = 'aurora.local'
 port = 1883
 wled = "wled/aurorawled"
 # generate client ID with pub prefix randomly
