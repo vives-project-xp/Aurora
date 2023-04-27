c9aeb0c2cf167bfd46472656e5c7928d17885db2
Tbo-s <thibautschroyens@gmail.com>
2023-04-27 14:33:09 +0200

    .

--- t_bowebsite/javascript/javascript.js
+++ t_bowebsite/javascript/javascript.js
@@ -8,30 +8,39 @@ var hexInput = document.getElementById("hex-input");
 var decimalInput = document.getElementById("decimal-input");
 var colorBox = document.getElementById("color-box");
 
+function onLoad() {
+}
+
+function Loop() {
+    timeout = setInterval(function () {
+        Sensors();
+    }, 1000);
+}
+
 function updateColor() {
-  var red = redSlider.value;
-  var green = greenSlider.value;
-  var blue = blueSlider.value;
-  var color = "rgb(" + red + "," + green + "," + blue + ")";
-  colorBox.style.backgroundColor = color;
-  redValue.innerText = red;
-  greenValue.innerText = green;
-  blueValue.innerText = blue;
-  hexInput.value = "#" + rgbToHex(red, green, blue);
-  decimalInput.value = rgbToDecimal(red, green, blue);
+    var red = redSlider.value;
+    var green = greenSlider.value;
+    var blue = blueSlider.value;
+    var color = "rgb(" + red + "," + green + "," + blue + ")";
+    colorBox.style.backgroundColor = color;
+    redValue.innerText = red;
+    greenValue.innerText = green;
+    blueValue.innerText = blue;
+    hexInput.value = "#" + rgbToHex(red, green, blue);
+    decimalInput.value = rgbToDecimal(red, green, blue);
 }
 
 function componentToHex(c) {
-  var hex = Number(c).toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
+    var hex = Number(c).toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
-  return componentToHex(r) + componentToHex(g) + componentToHex(b);
+    return componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 function rgbToDecimal(r, g, b) {
-  return r + ", " + g + ", " + b;
+    return r + ", " + g + ", " + b;
 }
 
 redSlider.addEventListener("input", updateColor);
@@ -39,27 +48,27 @@ greenSlider.addEventListener("input", updateColor);
 blueSlider.addEventListener("input", updateColor);
 
 function init() {
-  updateColor();
+    updateColor();
 }
-redSlider.addEventListener("mousemove", function(){
-  var x = redSlider.value;
-  var color = 'linear-gradient(90deg,red ' + x/255*100 + '%,grey ' + x/255*100 + '%)';
+redSlider.addEventListener("mousemove", function () {
+    var x = redSlider.value;
+    var color = 'linear-gradient(90deg,red ' + x / 255 * 100 + '%,grey ' + x / 255 * 100 + '%)';
 
-  redSlider.style.background = color;
+    redSlider.style.background = color;
 })
 
-greenSlider.addEventListener("mousemove", function(){
-  var x = greenSlider.value;
-  var color = 'linear-gradient(90deg,#00ff00 ' + x/255*100 + '%,grey ' + x/255*100 + '%)';
+greenSlider.addEventListener("mousemove", function () {
+    var x = greenSlider.value;
+    var color = 'linear-gradient(90deg,#00ff00 ' + x / 255 * 100 + '%,grey ' + x / 255 * 100 + '%)';
 
-  greenSlider.style.background = color;
+    greenSlider.style.background = color;
 })
 
-blueSlider.addEventListener("mousemove", function(){
-  var x = blueSlider.value;
-  var color = 'linear-gradient(90deg,blue ' + x/255*100 + '%,grey ' + x/255*100 + '%)';
+blueSlider.addEventListener("mousemove", function () {
+    var x = blueSlider.value;
+    var color = 'linear-gradient(90deg,blue ' + x / 255 * 100 + '%,grey ' + x / 255 * 100 + '%)';
 
-  blueSlider.style.background = color;
+    blueSlider.style.background = color;
 })
 
 init();
@@ -68,7 +77,7 @@ init();
 
 
 
-function Color(){
+function Color() {
     let data = {
         red: redSlider.value,
         green: greenSlider.value,
@@ -77,10 +86,48 @@ function Color(){
     Post("color", data);
 }
 
-function Toggle(){
+function Toggle() {
     Post("toggle", "");
 }
 
+async function Sensors() {
+    sensors = JSON.parse(await Post("sensors"));
+
+    //clearing sensor list
+    //document.getElementById("sensors").textContent = "";
+    for (sensor of sensors) {
+        AddSensor(sensor)
+    }
+}
+
+function AddSensor(sensor) {
+    const sensors = document.getElementById("sensors");
+    if (sensors.querySelector("#" + sensor["name"]) == null) {
+        const node = document.createElement('li');
+        const name = document.createElement("p");
+        name.innerHTML = sensor["name"];
+        name.setAttribute("id", sensor["name"]);
+        const id = document.createElement("input");
+        id.setAttribute("type", "text");
+        id.setAttribute("value", sensor["id"]);
+        id.setAttribute("onchange", 'UpdateSensor("' + sensor["name"] + '",this.value)');
+        const time = document.createTextNode(sensor["lastseen"]);
+        node.appendChild(name);
+        node.appendChild(id);
+        node.appendChild(time);
+
+        document.getElementById("sensors").appendChild(node);
+    }
+}
+
+function UpdateSensor(name, id) {
+    console.log("update " + name + " to " + id);
+    let data = {
+        name: name,
+        id: id
+    }
+    Post("updatesensor", data);
+}
 
 
 /*Form()
@@ -88,35 +135,38 @@ function Toggle(){
 test test
 {
     
-		const form = document.getElementById("preset-form");
-		form.addEventListener("submit", function(event) {
-			event.preventDefault(); 
+        const form = document.getElementById("preset-form");
+        form.addEventListener("submit", function(event) {
+            event.preventDefault(); 
       // Prevent the form from submitting and refreshing the page
 
-			const input = document.getElementById("preset-input");
-			const preset = input.value;
-			console.log("You gave a preset: " + preset);
+            const input = document.getElementById("preset-input");
+            const preset = input.value;
+            console.log("You gave a preset: " + preset);
 
-			// You can now use the value of `preset` to do whatever you need to do, such as sending it to a server or updating the UI
-		});
+            // You can now use the value of `preset` to do whatever you need to do, such as sending it to a server or updating the UI
+        });
 	
 }*/
 
 function Preset() {
-  let data = {ps: document.getElementById("preset").value}
-  Post("preset", data)
+    let data = { ps: document.getElementById("preset").value }
+    Post("preset", data)
 }
 
-function Post(page, data){
-  //fetch("http://" + document.getElementById("api").value + "/" + page, {
-  fetch("http://aurora.local:5500/" + page, {
-      method: 'post',
-      body: JSON.stringify(data),
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  })    
+async function Post(page, data) {
+    //fetch("http://" + document.getElementById("api").value + "/" + page, {
+    let response = await fetch("http://10.11.0.2:5500/" + page, {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    msg = await response.text();
+    return msg;
 }
+
 
 
 
