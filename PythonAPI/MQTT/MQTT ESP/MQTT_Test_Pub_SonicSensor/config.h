3b3f77bbd3c78c6b486bc91f0f85f667ab3d2ca8
Thomas Oddery <thomas.oddery@student.vives.be>
2023-05-11 09:40:07 +0200

    final push

---
+++ PythonAPI/MQTT/MQTT ESP/MQTT_Test_Pub_SonicSensor/config.h
@@ -0,0 +1,12 @@
+namespace Sonic {
+
+  namespace Config {
+
+    const char * WIFI_SSID =              "devbit";
+    const char * WIFI_PASSWORD =          "Dr@@dloos!";
+    const char * MQTT_BROKER_IP =         "broker.emqx.io";
+    const unsigned int MQTT_BROKER_PORT = 1883;
+    const String MQTT_BASE_TOPIC =        "aurora/sensor_1";
+
+  };
+};
